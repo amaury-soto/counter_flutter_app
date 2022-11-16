14ba9b867e42889bd715f3d459af1a031f2459ec
Amaury Soto Garay <amaury.soto@globant.com>
2022-11-16 16:06:54 -0500

    initial StatefulWidget component

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -1,35 +1,43 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
 
+  //Created state
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int counter = 17;
   @override
   Widget build(BuildContext context) {
     const fontSize = TextStyle(fontSize: 25);
     return Scaffold(
       //backgroundColor: Colors.indigo,
       appBar: AppBar(
-        title: const Text('Home Screen AppBar'),
+        title: const Text('Counter Screen AppBar'),
         elevation: 0,
       ),
       body: Center(
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const <Widget>[
-              Text('Clicks counters:', style: fontSize),
-              Text('10', style: fontSize),
+            children: <Widget>[
+              const Text('Clicks counters:', style: fontSize),
+              Text('$counter', style: fontSize),
             ],
           ),
         ),
       ),
       //floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          counter++;
+          setState(() {});
+        },
         child: const Icon(Icons.add),
-      
-
-      ) ,
+      ),
     );
   }
 }
